4b769bcf8665ee25e1b958d46f8a0afd1d54f4ee
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-19 22:24:20 -0400

    revert changes

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,6 @@
-require('dotenv').config()
-const { MONGODBURI } = process.env
 const mongoose = require('mongoose')
-const config = { useUnifiedTopology: true, useNewUrlParser: true }
-const DB = mongoose.connection
-
-mongoose.connect(MONGODBURI, config)
+mongoose.connect('mongodb://localhost/cookbooks_db', { useNewUrlParser: true })
 
 mongoose.Promise = Promise
-
-DB.on('open', () => console.log('You are connected to Mongo'))
-	.on('close', () => console.log('You are disconnected to Mongo'))
-	.on('error', (err) => console.log(err))
 
 module.exports = mongoose
--- server.js
+++ server.js
@@ -1,17 +1,8 @@
-require('dotenv').config()
-const { PORT = 3000, NODE_ENV = 'development' } = process.env
-
-const mongoose = require('./db/connection')
-
-const cors = require('cors')
-const corsOptions = require('./configs/cors.js')
-
 const express = require('express')
 const app = express()
 
 // Add the middleware code needed to accept incoming data and add it to req.body
 
-NODE_ENV === 'production' ? app.use(cors(corsOptions)) : app.use(cors())
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
@@ -26,4 +17,4 @@ app.use('/api/cookbooks/', cookbookRouter)
 const authorRouter = require('./controllers/authorRoutes')
 app.use('/api/authors/', authorRouter)
 
-app.listen(4000, () => console.log(`Port = ${PORT}`))
+app.listen(4000, () => console.log(`Port = 4000`))
