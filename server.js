4e6631b368f4893f0ddc44f97f8b09511ebb6e5c
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-19 22:14:53 -0400

    attempting to fix for deployment

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
+require('dotenv').config()
+const { PORT = 3000, NODE_ENV = 'development' } = process.env
+
+const mongoose = require('./db/connection')
+
+const cors = require('cors')
+const corsOptions = require('./configs/cors.js')
+
 const express = require('express')
 const app = express()
 
 // Add the middleware code needed to accept incoming data and add it to req.body
 
+NODE_ENV === 'production' ? app.use(cors(corsOptions)) : app.use(cors())
+
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.json({ hello: 'Hello World!' })
+})
 
 const cookbookRouter = require('./controllers/cookbookRoutes')
 app.use('/api/cookbooks/', cookbookRouter)
@@ -12,4 +26,4 @@ app.use('/api/cookbooks/', cookbookRouter)
 const authorRouter = require('./controllers/authorRoutes')
 app.use('/api/authors/', authorRouter)
 
-app.listen(4000, () => console.log('Server running on port 4000!'))
+app.listen(4000, () => console.log(`Port = ${PORT}`))
